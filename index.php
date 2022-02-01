95cca96e7c3074e982062db67b31ba205d64ac12
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-02-01 12:15:42 +0200

    modify index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
-if ($_SERVER['REQUEST_URI'] === '/') {
-echo '<a href="/welcome">welcome</a>
-<br>
-<a href="/not-found">not-found</a>';
-} elseif ($_SERVER['REQUEST_URI'] === '/welcome') {
-echo '<a href="/">main</a>';
-} else {
-header("HTTP/1.1 404 Not Found");
-echo 'Page not found. <a href="/">main</a>';
-}
+$links = [
+    ['name' => 'Google', 'url' => 'https://google.com'],
+    ['name' => 'Yandex', 'url' => 'https://yandex.com'],
+    ['name' => 'Bingo', 'url' => 'https://bingo.com']
+];
+foreach($links as $link) : ?>
+<div>
+<a href="<?= $link['url'] ?>"><?= $link['name'] ?></a>
+</div>
+<?php endforeach; ?>
